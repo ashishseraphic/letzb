d6617691781e708f46b2c6d549daba8b8aab5443
Ashish Kumar <ashish@Apples-MacBook-Air.local>
2020-08-24 09:58:34 +0530

    added engines and port

--- index.js
+++ index.js
@@ -1,78 +1,79 @@
-var express = require('express');
+var express = require("express");
 var app = express(),
-    path = require('path'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-	cors = require('cors'),
-	https = require("https"),
-    config = require('./server/config.server'),
-    nunjucks = require('nunjucks'),
-    multer = require('multer')
-    // fs = require('fs'),
-    // Storage = multer.diskStorage({
-	// 	destination: function (req, file, callback) {
-          
-    //         callback(null, path.join(process.env.PWD, 'uploads'));
-           
-    //     },
-      
-	// 	filename: function (req, file, callback) {
-    //         // console.log("File", file)
-	// 		callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
-	// 	}
-	// }),
-	// upload = multer({
-	// 	storage: Storage
-    // }),
-    // dir = path.join(process.env.PWD, 'server/uploads')
-          
-console.log("url",config.serverUrl);
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  cookieParser = require("cookie-parser"),
+  cors = require("cors"),
+  https = require("https"),
+  config = require("./server/config.server"),
+  nunjucks = require("nunjucks"),
+  multer = require("multer");
+// fs = require('fs'),
+// Storage = multer.diskStorage({
+// 	destination: function (req, file, callback) {
 
+//         callback(null, path.join(process.env.PWD, 'uploads'));
 
-require('./server/models');
-app.set('views', path.join(__dirname, 'server/views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+//     },
 
-const routes = require('./server/routes');
+// 	filename: function (req, file, callback) {
+//         // console.log("File", file)
+// 		callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+// 	}
+// }),
+// upload = multer({
+// 	storage: Storage
+// }),
+// dir = path.join(process.env.PWD, 'server/uploads')
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+console.log("url", config.serverUrl);
+
+require("./server/models");
+app.set("views", path.join(__dirname, "server/views"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+const routes = require("./server/routes");
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
 
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'))
-app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(process.env.PWD, "public")));
 
-app.use(express.static(__dirname + '/uploads'));
+app.use(express.static(__dirname + "/uploads"));
 
 // Allow CORS
-app.use(cors({
-    'origin': "*",
-    'exposedHeaders': ['X-Requested-With', 'content-type', 'Authorization'],
-    'credentials': true
-}))
+app.use(
+  cors({
+    origin: "*",
+    exposedHeaders: ["X-Requested-With", "content-type", "Authorization"],
+    credentials: true,
+  })
+);
 
-nunjucks.configure('server/views', {
-    autoescape: true,
-    express: app
+nunjucks.configure("server/views", {
+  autoescape: true,
+  express: app,
 });
 
-const fixtures = require('./server/fixtures');
-const router = require('./server/routes/promotion.server.routes');
+const fixtures = require("./server/fixtures");
+const router = require("./server/routes/promotion.server.routes");
 fixtures.userFixture.fixtureUser();
 
-app.use('/', routes.users);
-app.use('/', routes.questions);
-app.use('/', routes.swipe);
-app.use('/', routes.profiles);
-app.use('/', routes.categories);
-app.use('/', routes.eventlocation);
-app.use('/', routes.events);
-app.use('/', routes.admin);
-app.use('/', routes.privacy);
-app.use('/', routes.promotion)
-app.use('/', routes.feedback)
-app.use('/', routes.subscription)
+app.use("/", routes.users);
+app.use("/", routes.questions);
+app.use("/", routes.swipe);
+app.use("/", routes.profiles);
+app.use("/", routes.categories);
+app.use("/", routes.eventlocation);
+app.use("/", routes.events);
+app.use("/", routes.admin);
+app.use("/", routes.privacy);
+app.use("/", routes.promotion);
+app.use("/", routes.feedback);
+app.use("/", routes.subscription);
 
 // const  {admin} = require('./service/pushnotification')
 
@@ -89,7 +90,7 @@ app.use('/', routes.subscription)
 //         priority: "high",
 //         timeToLive: 60 * 60 *24
 //       };
-      
+
 //       admin.messaging().sendToDevice(registrationToken, payload, options)
 //         .then(function(response) {
 //             console.log(response.results[0].error);
@@ -100,10 +101,11 @@ app.use('/', routes.subscription)
 //         });
 // })
 
-app.get('/', function (err, res) {
-    res.send("Hello There")
-})
+app.get("/", function (err, res) {
+  res.send("Hello There");
+});
 
-app.listen(5000, () => {
-    console.log("server started on 5000");
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server started on ${port}`);
+});
