1cfd1c5cefcf178cbaf452a8dda25fa2e60d766f
Ashish Kumar <ashish@Apples-MacBook-Air.local>
2020-08-25 09:57:15 +0530

    added mongo uri

--- server/models/db.server.connect.js
+++ server/models/db.server.connect.js
@@ -1,23 +1,28 @@
-'use strict';
+"use strict";
 
-let mongoose = require('mongoose');
-var url = "mongodb://localhost:27017/letzb";
+require("dotenv/config");
 
-mongoose.connect(url, {
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}, (error) => {
-  console.log('connection error', error)
-});
+let mongoose = require("mongoose");
+// var url = "mongodb://localhost:27017/letzb";
+const url = process.env.MONGO_URI;
 
+mongoose.connect(
+  url,
+  {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  },
+  (error) => {
+    console.log("connection error", error);
+  }
+);
 
-let timestamps = require('mongoose-timestamp');
+let timestamps = require("mongoose-timestamp");
 
 mongoose.plugin(timestamps, {
-  createdAt: 'created_at',
-  updatedAt: 'modified_at'
-})
+  createdAt: "created_at",
+  updatedAt: "modified_at",
+});
 
 module.exports = mongoose;
-
-
